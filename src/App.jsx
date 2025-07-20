08036a637b68b23ce4c4192e97dbdd34fb7306d9
Polok Banik <polokbanik7@gmail.com>
2025-07-20 16:14:42 +0600

    final push

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,23 @@
+import React from "react";
+import Navbar from "./components/Navbar/Navbar";
+import Hero from "./components/Hero/Hero";
+import Products from "./components/Products/Products";
 import AOS from "aos";
 import "aos/dist/aos.css";
-import { useEffect, useState } from "react";
+import TopProducts from "./components/TopProducts/TopProducts";
 import Banner from "./components/Banner/Banner";
-import Navbar from "./components/Navbar/Navbar";
+import Subscribe from "./components/Subscribe/Subscribe";
+import Testimonials from "./components/Testimonials/Testimonials";
+import Footer from "./components/Footer/Footer";
+import Popup from "./components/Popup/Popup";
 
-export default function App() {
-  const [orderPopup, setOrderPopup] = useState(false);
+const App = () => {
+  const [orderPopup, setOrderPopup] = React.useState(false);
 
   const handleOrderPopup = () => {
     setOrderPopup(!orderPopup);
   };
-
-  useEffect(() => {
+  React.useEffect(() => {
     AOS.init({
       offset: 100,
       duration: 800,
@@ -20,11 +26,20 @@ export default function App() {
     });
     AOS.refresh();
   }, []);
+
   return (
-    <>
-      <div className="bg-white dark:bg-gray-900 dark:text-white duration-200 ">
-        <Navbar handleOrderPopup={handleOrderPopup} />
-      </div>
-    </>
+    <div className="bg-white dark:bg-gray-900 dark:text-white duration-200">
+      <Navbar handleOrderPopup={handleOrderPopup} />
+      <Hero handleOrderPopup={handleOrderPopup} />
+      <Products />
+      <TopProducts handleOrderPopup={handleOrderPopup} />
+      <Banner />
+      <Subscribe />
+      <Testimonials />
+      <Footer />
+      <Popup orderPopup={orderPopup} setOrderPopup={setOrderPopup} />
+    </div>
   );
-}
+};
+
+export default App;
--- src/components/Navbar/DarkMode.jsx
+++ src/components/Navbar/DarkMode.jsx
@@ -1,15 +1,15 @@
-import { useEffect, useState } from "react";
+import React from "react";
 import LightButton from "../../assets/website/light-mode-button.png";
 import DarkButton from "../../assets/website/dark-mode-button.png";
 
-export default function DarkMode() {
-  const [theme, setTheme] = useState(
+const DarkMode = () => {
+  const [theme, setTheme] = React.useState(
     localStorage.getItem("theme") ? localStorage.getItem("theme") : "light"
   );
 
-  const element = document.documentElement;
+  const element = document.documentElement; // html element
 
-  useEffect(() => {
+  React.useEffect(() => {
     if (theme === "dark") {
       element.classList.add("dark");
       localStorage.setItem("theme", "dark");
@@ -17,29 +17,26 @@ export default function DarkMode() {
       element.classList.remove("dark");
       localStorage.setItem("theme", "light");
     }
-  });
+  }, [theme]);
 
   return (
     <div className="relative">
       <img
         src={LightButton}
-        alt="light button"
-        onClick={() => {
-          setTheme(theme === "light" ? "dark" : "light");
-        }}
+        alt=""
+        onClick={() => setTheme(theme === "light" ? "dark" : "light")}
         className={`w-12 cursor-pointer drop-shadow-[1px_1px_1px_rgba(0,0,0,0.1)] transition-all duration-300 absolute right-0 z-10 ${
-          theme == "dark" ? "opacity-0" : "opacity-100"
-        }`}
+          theme === "dark" ? "opacity-0" : "opacity-100"
+        } `}
       />
-
       <img
         src={DarkButton}
-        alt="dark button"
-        onClick={() => {
-          setTheme(theme == "light" ? "dark" : "light");
-        }}
+        alt=""
+        onClick={() => setTheme(theme === "light" ? "dark" : "light")}
         className="w-12 cursor-pointer drop-shadow-[1px_1px_1px_rgba(0,0,0,0.1)] transition-all duration-300"
       />
     </div>
   );
-}
+};
+
+export default DarkMode;
